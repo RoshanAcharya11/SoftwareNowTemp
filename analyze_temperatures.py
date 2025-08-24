dc5974e48d7b5f28e8956ae9612ef3fc81ce0a2f
RoshanAcharya11 <164843783+RoshanAcharya11@users.noreply.github.com>
2025-08-24 21:43:14 +1000

    New Changes analyze_temperatures.py
    Addition in read_csv_file function

--- analyze_temperatures.py
+++ analyze_temperatures.py
@@ -14,17 +14,26 @@ def read_csv_files(folder_path="temperatures"):
     if not os.path.exists(folder_path):
         raise FileNotFoundError(f"Folder '{folder_path}' does not exist.")
     data = []
+    months = sum (SEASONS.values(), [])
     for filename in os.listdir(folder_path):
         if filename.endswith('.csv'):
             with open(os.path.join(folder_path, filename), 'r', encoding='utf-8') as f:
                 reader = csv.DictReader(f)
-                data.extend(list(reader))
+                if not all(col in reader.fieldnames for col in ['STATION_NAME'] + months):
+                    continue
+                data.extend([{month: float(row[month]) if row [month] else None for month in months} | {'STATION_NAME' : row['STATION_NAME']} for row in reader])
+    if not data:
+        raise ValueError("No Valid data found.")
     return data
+    
+def analyze_temperatures(data):
+    return
 
 def main():
     """Execute temperature analysis."""
     try:
         data = read_csv_files()
+        results = analyze_temperatures(data)
         print(f"Read {len(data)} station records.")
         # TODO: Validate data, add analysis functions
     except Exception as e:
@@ -32,3 +41,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
